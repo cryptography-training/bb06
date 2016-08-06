227bafe1f3d065b080d3d77652534e1c17c88490
Filippo Valsorda <filippo@cloudflare.com>
2016-08-06 12:59:34 -0700

    Add utils to client

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import requests
 import binascii
+import math
+from gmpy2 import mpz, iroot
 
 def send(name, sig):
     url = "http://localhost:4242/api"
@@ -8,4 +10,26 @@ def send(name, sig):
         'name': name,
     }
     return requests.get(url, params=data)
+
+def to_bytes(n):
+    """ Return a bytes representation of a int """
+    return n.to_bytes((n.bit_length() // 8) + 1, byteorder='big')
+
+def from_bytes(b):
+    """ Makes a int from a bytestring """
+    return int.from_bytes(b, byteorder='big')
+
+def get_bit(n, b):
+    """ Returns the b-th rightmost bit of n """
+    return ((1 << b) & n) >> b
+
+def set_bit(n, b, x):
+    """ Returns n with the b-th rightmost bit set to x """
+    if x == 0: return ~(1 << b) & n
+    if x == 1: return (1 << b) | n
+
+def cube_root(n):
+    return int(iroot(mpz(n), 3)[0])
+
+
 
